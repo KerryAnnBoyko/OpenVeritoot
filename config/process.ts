53f002d0c19c699878b6653c48584e8f3b896385
KerryAnnBoyko <KABoykoTemp@gmail.com>
2022-11-26 17:38:15 -0500

    Add basics of next-auth

---
+++ config/process.ts
@@ -0,0 +1,4 @@
+export const githubCredentials = {
+  clientId: process.env.GITHUB_ID || "",
+  clientSecret: process.env.GITHUB_SECRET || "",
+};
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,3 +1,10 @@
 import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github';
 
-export const X = () => null;
+import { githubCredentials } from '@/config/process';
+
+export const authOptions = {
+  providers: [
+    GithubProvider(githubCredentials)
+  ]
+}
