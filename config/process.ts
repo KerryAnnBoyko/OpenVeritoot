476d49b2a3eb9539a8e4429b92ebc585f6021292
KerryAnnBoyko <KABoykoTemp@gmail.com>
2022-11-27 20:20:37 -0500

    Prisma client setup

--- config/process.ts
+++
@@ -1,6 +0,0 @@
-export const githubCredentials = {
-  clientId: process.env.GITHUB_ID || "",
-  clientSecret: process.env.GITHUB_SECRET || "",
-};
-
-export const databaseURL = process.env.DATABASE_URL;
